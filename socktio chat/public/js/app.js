4bfb7c08d78c4403c8e466ac64a670544d1142ce
rsol7 <rsolm72@gmail.com>
2018-12-24 18:48:40 -0700

    uploading photo, and plugging it in chat by sending blob through socket to other clients. Also, required proper structure expeting image. Currently facing issues with Edge browser not setting the image's class, works for chrome and firefox though.

--- socktio chat/public/js/app.js
+++ socktio chat/public/js/app.js
@@ -1,9 +1,11 @@
 $(() => {
     const socket = io();
+    var user_pic;
 
     $('form').submit(() => {
         socket.emit('chat message', 
         {
+            "user_pic": user_pic,
             "user": $("#username").val(),
             "msg": $('#message').val()
         });
@@ -13,11 +15,48 @@ $(() => {
     });
 
     socket.on('chat message', data => {
+        
+        var list_item = `
+            <img src=${data.user_pic} width=50; height=50; />
+            ${data.user}: ${data.msg}
+        `;
+
         $('#messages')
-            .append($('<li>')
-            .text(data.user+": " + data.msg))
-            .scrollTop( $("#messages").prop("scrollHeight") );
-        // chatWindow.scrollTop = chatWindow.scrollHeight;
+            .append( 
+                $('<li>').append([
+                    $("<img/>",{"src": data.user_pic, "class": "list-item-img"}),
+                    $("<h5/>", {"text": data.user}),
+                    $("<p/>", {"text": data.msg})
+                ])
+            )
+            .scrollTop($("#messages").prop("scrollHeight"));
+
+        // $('#messages')
+        //     .append( $('<li>').html(list_item))
+        //     .scrollTop($("#messages").prop("scrollHeight"));
     });
 
+ ///////// uploading file
+
+    $("#photouploader").bind('change', previewFile);
+
+    function previewFile() {
+        var preview = document.querySelector('img');
+        var file    = document.querySelector('input[type=file]').files[0];
+        var reader  = new FileReader();
+    
+        reader.addEventListener("load", function () {
+            user_pic = reader.result;
+            preview.src = reader.result;
+        }, false);
+    
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+
+
 });
+
+
+   
