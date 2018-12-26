b892250a59a1acf2040becbac2cc653c19757c68
rsol7 <rsolm72@gmail.com>
2018-12-26 02:49:52 -0700

    change array buffer reading to dataurl reading

--- socktio chat/public/js/app.js
+++ socktio chat/public/js/app.js
@@ -48,15 +48,15 @@ $(() => {
     function startRead() {
         let file = document.querySelector('input[type=file]').files[0];
         if(file){
-            getAsArrayBuffer(file);
+            getAsDataURL(file);
         }
     }
 
-    function getAsArrayBuffer(readFile) {
+    function getAsDataURL(readFile) {
         const reader = new FileReader();
 
-        // Read file into memory as ArrayBuffer
-        reader.readAsArrayBuffer(readFile);
+        // Read file into memory as DataURL
+        reader.readAsDataURL(readFile);
 
         // Handle progress, success, and errors
         reader.onloadstart = loadstart;
@@ -83,9 +83,7 @@ $(() => {
         console.log("load obj", evt);  // shows loaded progress of reader.result
         // Obtain the read file data with evt.target.result
 
-        let bytes = new Uint8Array(evt.target.result);
-        let base64 = 'data:image/png;base64,'+ encode(bytes);
-
+        let base64 = evt.target.result;
         preview.src =  base64;
         setTimeout( () => user_pic = base64, 0);
     }
@@ -100,34 +98,6 @@ $(() => {
             // The file could not be read
             console.log("error", evt.target.error);
         }
-    }
-
-    // public method for encoding an Uint8Array to base64
-    function encode (input) {
-        var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-        var output = "";
-        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-        var i = 0;
-
-        while (i < input.length) {
-            chr1 = input[i++];
-            chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index 
-            chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
-
-            enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
-
-            if (isNaN(chr2)) {
-                enc3 = enc4 = 64;
-            } else if (isNaN(chr3)) {
-                enc4 = 64;
-            }
-            output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-                    keyStr.charAt(enc3) + keyStr.charAt(enc4);
-        }
-        return output;
     }
 
 });
